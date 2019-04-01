5fdb82a7133a89e303da388c396833818bfe6386
oscar <oscar@o2webdev.com>
2019-04-01 11:37:58 -0400

    change var to let

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 'use strict';
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
+let loopback = require('loopback');
+let boot = require('loopback-boot');
 
-var app = module.exports = loopback();
+let app = module.exports = loopback();
 
 app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
+    let baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      let explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
